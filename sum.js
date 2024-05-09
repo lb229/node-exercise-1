d03e4f3d5bd4bff1f7884307b81c0b31ee3f83a7
Lucy Lainez <156219119+lb229@users.noreply.github.com>
2024-05-09 16:20:29 +0200

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,6 @@
+/* Exercise 1: Create a sum script
+
+Create a script that outputs the total of 5 + 6 when you run it with Node.js. */
+
+const sum = 5 + 6;
+console.log(`the sum of 5 and 6 is: ${sum}`);
